eed3b543e05782b8f29829dcd91d95621df27b4a
MateoDi9z <dzmateozd@protonmail.com>
2026-09-14 09:48:17 -0300

    feat: added infra to CD

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.serialization)
+
+    // Continuous Deployment
+    id("printscript.publishing-conventions")
 }
 
 repositories {
