cdbf23cb571577f962d57b9e612e89a223c52993
MateoDi9z <dzmateozd@protonmail.com>
2026-09-21 19:46:37 -0300

    fix: tokensource import

--- parser/src/test/kotlin/printscript/token/LexerTokenSourceTest.kt
+++ parser/src/test/kotlin/printscript/token/LexerTokenSourceTest.kt
@@ -3,6 +3,7 @@ package printscript.token
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import printscript.parser.token.LexerTokenSource
 import printscript.support.MockLexer
 import printscript.support.Tokens
 
